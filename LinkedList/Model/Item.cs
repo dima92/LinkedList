3dbb064f533d93938fd7d7cb35d43f56a3f37883
Дмитрий <dima-tkachenko@mail.ru>
2020-01-31 01:06:54 +0300

    Добавьте файлы проекта.

---
+++ LinkedList/Model/Item.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LinkedList.Model
+{
+    /// <summary>
+    /// Ячейка списка.
+    /// </summary>
+    public class Item<T>
+    {
+        private T _data;
+
+        /// <summary>
+        /// Данные хранимые в ячейке списка.
+        /// </summary>
+        public T Data
+        {
+            get => _data;
+            set
+            {
+                if (value != null)
+                    _data = value;
+                else
+                    throw new ArgumentNullException(nameof(value)); }
+        }
+
+        /// <summary>
+        /// Следующая ячейка списка
+        /// </summary>
+        public Item<T> Next { get; set; }
+
+        public Item(T data)
+        {
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,50 @@
+﻿using LinkedList.Model;
+using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var list = new LinkedList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            list.Delete(3);
+            list.Delete(1);
+            list.Delete(7);
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            list.AppendHead(7);
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            list.InsertAfter(4, 8);
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
